82efe9d60e7af4b91b56cd1584a477c8896e5697
Cristian Valentin <crisvalentin891@gmail.com>
2022-09-05 04:15:48 -0400

    anadimos funciones al codigo tales como funciones y mejoras al valor de dolares

--- conversorMonedas.py
+++ conversorMonedas.py
@@ -1,9 +1,26 @@
-valor_EUR = 1.009558 #Euro
+valor_EUR = 1.01 #Euro
+valor_USD = 1.00 #USD
+valor_CAD = 1.31 #CAD
 
-dolarUSD = input("Cuanto Dinero USD tiene?: ")
-dolarUSD = float(dolarUSD)
+def conversion(dollarType, dollarValue, finalConv):
+    type = float(input("Cuanto Dinero " + dollarType + " tiene?: "))
+    dollar = (type * dollarValue)
+    dollar = round(dollar, 2)
+    dollar = str(dollar)
+    print("Tendrias " + dollar + finalConv)
 
-EUR = (dolarUSD * valor_EUR)
-EUR = round(EUR, 2)
-EUR = str(EUR)
-print("Tienes " + EUR + " EUR")
+menu = """A que moneda desea convertir? \n1. USD to EUR\n2.EUR to USD\n3.USD to CAD\n4.CAD to USD
+Elige una opcion: """
+userSelection = int(input(menu))
+
+if userSelection == 1:
+    conversion("USD", 1.01, " EUR")
+
+elif userSelection == 2:
+    conversion("EUR", .99, " USD")
+
+elif userSelection == 3:
+    conversion("USD", 1.31, "CAD")
+
+elif userSelection == 4:
+    conversion("CAD", .76, "USD")
